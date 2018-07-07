16bfe12c9af6aa88dd3807371718f3f26be307de
monkeygq <hgq6389465@gmail.com>
2018-07-08 01:01:37 +0800

    string usage

--- string.cpp
+++ string.cpp
@@ -33,7 +33,35 @@ int main() {
     print_char(str[0]); /* h */
 
 	/* a pointer to s[n] can be passed to functions that expect a char*    */
-	print_char_ptr(&str[0]); /* e */
-	std::cout << str << std::endl;
+	print_char_ptr(&str[0]); /* h */
+	std::cout << str << std::endl; /* hel3o */
+
+	std::string s1;
+	std::string s2(5, 'b');
+	std::cout << s2 << std::endl; /* bbbbb */
+
+	/* constructs the string with a substring [pos, pos+count) of other string*/
+	std::string s3(s2, 1, 3); /* other string:s2, pos:1, count:3 */
+	std::cout << s3 << std::endl; /* bbb */
+
+	char ch[] = "hello world";
+	/* Constructs the string with the first count characters of char*    */
+	std::string s4(ch, 8); /* count:8 */
+	std::cout << s4 << std::endl; /* hello wo */
+
+	/* constructs the string with char*    */
+	std::string s5(ch);
+	std::cout << s5 << std::endl; /* hello world */
+
+	std::string s6(s5.begin(), s5.begin() + 3);
+	std::cout << s6 << std::endl; /* hel */
+
+	std::string s7(s5);
+	std::cout << s7 << std::endl; /* hello world */
+
+	/* move constructor, right hand side reference */
+	std::string s8(std::string("hello") + std::string("world"));
+	std::cout << s8 << std::endl; /* helloworld */
+
     return 0;
 }
